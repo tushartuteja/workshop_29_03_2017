480103b928ae710da678a5e150405ce54bc26aaa
Tushar Tuteja <ttuteja@gmail.com>
2017-03-30 12:52:38 +0530

    Add files via upload

--- todo.rb
+++ todo.rb
@@ -12,7 +12,31 @@ end
 
 post '/add_task' do
 	task = params["task"]
-	all_tasks << task
+	task_hash = {}
+	task_hash["content"] = task
+	task_hash["completed"] = false
+	all_tasks << task_hash
 	puts all_tasks
 	return redirect '/'
 end
+
+post '/mark_complete' do
+	content = params["content"]
+	all_tasks.each do |task|
+		if task["content"] == content
+			task["completed"] = true
+		end
+	end
+	return redirect '/'
+
+end
+
+
+
+
+
+
+
+
+
+
